23d8d6991de6b524f1f962f9de2966447a08db4f
mdleiton <mau_leiton96@hotmail.com>
2019-04-02 11:16:37 -0500

    [upd] recibiendo datos de latitud, longitud

--- app/src/main/java/com/projects/mirai/koukin/pruebasmapa/ConfigActivity.java
+++ app/src/main/java/com/projects/mirai/koukin/pruebasmapa/ConfigActivity.java
@@ -1,54 +1,31 @@
 package com.projects.mirai.koukin.pruebasmapa;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.MapFragment;
-import com.projects.mirai.koukin.pruebasmapa.HelperClass.SBPDriverJ2XX;
-import com.projects.mirai.koukin.pruebasmapa.HelperClass.Utils;
-import com.swiftnav.sbp.SBPMessage;
-import com.swiftnav.sbp.client.SBPCallback;
-import com.swiftnav.sbp.client.SBPHandler;
-import com.swiftnav.sbp.loggers.JSONLogger;
-import com.swiftnav.sbp.logging.MsgLog;
-import com.swiftnav.sbp.navigation.MsgPosLLH;
-import com.swiftnav.sbp.observation.MsgObs;
+import com.projects.mirai.koukin.pruebasmapa.HelperClass.SerialLink;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
 
 public class ConfigActivity extends AppCompatActivity {
-
-    /* piksi */
-    String TAG = "PiksiDroid";
-    String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
-    SBPHandler piksiHandler;
-    SBPDriverJ2XX piksiDriver;
-
-
 
     EditText txt_email,txt_ftp;
     RadioButton rb_gps,rb_rtk;
     Button guardar;
+    SerialLink piksi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,149 +62,32 @@ public class ConfigActivity extends AppCompatActivity {
             rb_gps.setChecked(true);
         }else{
             rb_rtk.setChecked(true);
-        }
+            piksi = new SerialLink(this.getApplicationContext());
+            //que pasa si no me puedo conectar
+            piksi.start();
+            @SuppressLint("HandlerLeak") Handler h = new Handler() {
+                public void handleMessage(Message msg){
+                    while(true){
+                        String data = "Nuevo datos del piksi -> lat: " + piksi.getLat() + ", log: "+ piksi.getLon();
+                        Toast.makeText(getApplicationContext(),data, Toast.LENGTH_LONG).show();
+                        try {
+                            TimeUnit.SECONDS.sleep(2);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
 
-        /* piksi */
-        System.out.println("1");
-        UsbManager mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        System.out.println("2");
-        PendingIntent mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
-        System.out.println("3");
-        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
-        System.out.println("4");
-        registerReceiver(mUsbReceiver, filter);
-        System.out.println("5");
-        filter = new IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED);
-        System.out.println("6");
-        registerReceiver(mUsbReceiverDisconnect, filter);
 
-        HashMap<String, UsbDevice> deviceList = mUsbManager.getDeviceList();
-
-        for (UsbDevice device : deviceList.values()) {
-            if ((device.getVendorId() == Utils.PIKSI_VID) && (device.getProductId() == Utils.PIKSI_PID))
-                if (!mUsbManager.hasPermission(device)){
-                    mUsbManager.requestPermission(device, mPermissionIntent);
-                    System.out.println("sin permiso conectado");
                 }
-                else {
-                   // ((EditText) findViewById(R.id.console)).setText("");
-                    System.out.println("con permisos");
-                    piksiConnected(device);
-                }
+            };
         }
-
-
-
     }
 
-    private void piksiConnected(UsbDevice usbdev) {
-        if (piksiDriver != null) {
-            piksiHandler.stop();
-            piksiDriver.close();
-            piksiDriver = null;
-        }
-        try {
-            System.out.println("intento serial");
-            piksiDriver = new SBPDriverJ2XX(this, usbdev);
-        } catch (IOException e) {
-            Log.d(TAG, e.toString());
-            e.printStackTrace();
-            return;
-        }
-        System.out.println("creando piksi");
-        piksiHandler = new SBPHandler(piksiDriver);
-
-        try {
-            File logfile = new File(getExternalFilesDir("logs"), "logfile");
-            OutputStream logstream = new FileOutputStream(logfile);
-            piksiHandler.addCallback(new JSONLogger(logstream));
-        } catch (Exception e) {
-            Log.e(TAG, "Error opening JSON log file: " + e.toString());
-        }
-
-        Log.d(TAG, "All ready to go...");
-        System.out.println("iniciando piksi");
-
-        piksiHandler.addCallback(MsgLog.TYPE, new SBPCallback() {
-            @Override
-            public void receiveCallback(SBPMessage msg) {
-                if (piksiHandler == null) {
-                    Log.e(TAG, "No piksi to send to!");
-                    return;
-                }
-                MsgLog msg_ = (MsgLog) msg;
-                System.out.print(msg_.text + ".\n");
-                //piksiHandler.send(msg);
-            }
-         });
-
-        piksiHandler.addCallback(MsgPosLLH.TYPE, new SBPCallback() {
-            @Override
-            public void receiveCallback(SBPMessage msg) {
-                if (piksiHandler == null) {
-                    Log.e(TAG, "No piksi to send to!");
-                    return;
-                }
-                MsgPosLLH msg__ = (MsgPosLLH)msg;
-                System.out.printf(
-                        "lat[deg]: %f, lon[deg]: %f, ellipsoid alt[m]: %f, horizontal accuracy[m]: %f, vertical_accuracy[m]: %f, n_sats: %d .\n",
-                        msg__.lat,
-                        msg__.lon,
-                        msg__.height,
-                        msg__.h_accuracy / 1000.0,
-                        msg__.v_accuracy / 1000.0,
-                        msg__.n_sats);
-                //piksiHandler.send(msg);
-            }
-        });
-        piksiHandler.start();
-
-    }
-
-    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (ACTION_USB_PERMISSION.equals(action)) {
-                UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-                if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-                    if (device != null) {
-                        //((EditText) findViewById(R.id.console)).setText("");
-                        System.out.println("algo conectado");
-                        piksiConnected(device);
-                    }
-                } else {
-                    System.out.println("nada conectado sin permiso");
-                    Log.e(TAG, "Permission denied for device " + device);
-                }
-            }
-        }
-    };
-
-    BroadcastReceiver mUsbReceiverDisconnect = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
-                UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-                if (device != null) {
-                    Log.e(TAG, "Device disconnected!");
-                    System.out.println("Device disconnected!");
-                    if (piksiDriver != null) {
-                        piksiHandler.stop();
-                        piksiDriver.close();
-                        piksiDriver = null;
-                        System.out.println("Piksi not connected!");
-                        //((EditText) findViewById(R.id.console)).setText("Piksi not connected!");
-                    }
-                }
-            }
-        }
-    };
 
     @Override
     protected void onDestroy() {
+        piksi.destroy();
         super.onDestroy();
-        unregisterReceiver(mUsbReceiver);
-        unregisterReceiver(mUsbReceiverDisconnect);
     }
 
     @Override
--- app/src/main/java/com/projects/mirai/koukin/pruebasmapa/HelperClass/SerialLink.java
+++ app/src/main/java/com/projects/mirai/koukin/pruebasmapa/HelperClass/SerialLink.java
@@ -1,16 +1,189 @@
 package com.projects.mirai.koukin.pruebasmapa.HelperClass;
 
-import com.swiftnav.sbp.client.SBPHandler;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
 
+import com.swiftnav.sbp.SBPMessage;
+import com.swiftnav.sbp.client.SBPCallback;
+import com.swiftnav.sbp.client.SBPHandler;
+import com.swiftnav.sbp.loggers.JSONLogger;
+import com.swiftnav.sbp.logging.MsgLog;
+import com.swiftnav.sbp.navigation.MsgPosLLH;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class SerialLink {
+    String TAG = "PiksiCVR";
+    String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    SBPHandler piksiHandler;
+    SBPDriverJ2XX piksiDriver;
+    Context context;
     private SBPHandler handler;
-    //private SBPFramer framer;
     private String[] fix_type = new String[8];
-    public Queue<Double> lat_queue = new LinkedList<>();
-    public Queue<Double> lon_queue = new LinkedList<>();
+    private Queue<Double> lat_queue = new LinkedList<>();
+    private Queue<Double> lon_queue = new LinkedList<>();
 
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                    if (device != null) {
+                        System.out.println("algo conectado");
+                        piksiConnected(device);
+                    }
+                } else {
+                    System.out.println("nada conectado sin permiso");
+                    Log.e(TAG, "Permission denied for device " + device);
+                }
+            }
+        }
+    };
+
+    BroadcastReceiver mUsbReceiverDisconnect = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+            UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+            if (device != null) {
+                Log.e(TAG, "Device disconnected!");
+                System.out.println("Device disconnected!");
+                if (piksiDriver != null) {
+                    piksiHandler.stop();
+                    piksiDriver.close();
+                    piksiDriver = null;
+                    System.out.println("Piksi not connected!");
+                }
+            }
+        }
+        }
+    };
+
+    public SerialLink(Context context) {
+        this.context = context;
+        detect_piksi();
+    }
+
+    private void detect_piksi() {
+        UsbManager mUsbManager = (UsbManager) this.context.getSystemService(Context.USB_SERVICE);
+        PendingIntent mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        context.registerReceiver(mUsbReceiver, filter);
+        filter = new IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        context.registerReceiver(mUsbReceiverDisconnect, filter);
+
+        HashMap<String, UsbDevice> deviceList = mUsbManager.getDeviceList();
+
+        for (UsbDevice device : deviceList.values()) {
+            if ((device.getVendorId() == Utils.PIKSI_VID) && (device.getProductId() == Utils.PIKSI_PID))
+                if (!mUsbManager.hasPermission(device)) {
+                    mUsbManager.requestPermission(device, mPermissionIntent);
+                    System.out.println("sin permiso conectado");
+                } else {
+                    System.out.println("con permisos");
+                    piksiConnected(device);
+                }
+        }
+    }
+
+    private void piksiConnected(UsbDevice usbdev) {
+        if (piksiDriver != null) {
+            piksiHandler.stop();
+            piksiDriver.close();
+            piksiDriver = null;
+        }
+        try {
+            System.out.println("intento serial");
+            piksiDriver = new SBPDriverJ2XX(context, usbdev);
+        } catch (IOException e) {
+            Log.d(TAG, e.toString());
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("creando piksi");
+        piksiHandler = new SBPHandler(piksiDriver);
+
+        try {
+            File logfile = new File(context.getExternalFilesDir("logs"), "logfile");
+            OutputStream logstream = new FileOutputStream(logfile);
+            piksiHandler.addCallback(new JSONLogger(logstream));
+        } catch (Exception e) {
+            Log.e(TAG, "Error opening JSON log file: " + e.toString());
+        }
+
+        Log.d(TAG, "All ready to go...");
+        System.out.println("iniciando piksi");
+
+        piksiHandler.addCallback(MsgLog.TYPE, new SBPCallback() {
+            @Override
+            public void receiveCallback(SBPMessage msg) {
+                if (piksiHandler == null) {
+                    Log.e(TAG, "No piksi to send to!");
+                    return;
+                }
+                MsgLog msg_ = (MsgLog) msg;
+                System.out.print(msg_.text + ".\n");
+                //piksiHandler.send(msg);
+            }
+        });
+
+        piksiHandler.addCallback(MsgPosLLH.TYPE, new SBPCallback() {
+            @Override
+            public void receiveCallback(SBPMessage msg) {
+                if (piksiHandler == null) {
+                    Log.e(TAG, "No piksi to send to!");
+                    return;
+                }
+                MsgPosLLH msg__ = (MsgPosLLH) msg;
+                lat_queue.add(msg__.lat);
+                lon_queue.add(msg__.lon);
+                System.out.printf(
+                        ", lat[deg]: %f, lon[deg]: %f, ellipsoid alt[m]: %f, n_sats: %d .\n",
+                        msg__.lat,
+                        msg__.lon,
+                        msg__.height,
+                        msg__.n_sats);
+                //piksiHandler.send(msg);
+            }
+        });
+    }
+
+    public void start(){
+        piksiHandler.start();
+    }
+    public void destroy() {
+        context.unregisterReceiver(mUsbReceiver);
+        context.unregisterReceiver(mUsbReceiverDisconnect);
+    }
+
+    public double getLat() {
+        if (!lat_queue.isEmpty()){
+            return lat_queue.poll();
+        }else{
+            return -1;
+        }
+    }
+
+    public double getLon() {
+        if (!lon_queue.isEmpty()){
+            return lon_queue.poll();
+        }else{
+            return -1;
+        }
+    }
 
 }
+
