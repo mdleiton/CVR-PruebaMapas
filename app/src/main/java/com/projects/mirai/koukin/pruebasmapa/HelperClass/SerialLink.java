9a2d3976bb4e717ed6c57094c20e758c578d7ad4
mdleiton <mau_leiton96@hotmail.com>
2019-06-14 03:26:37 -0500

    [upd] validaciones cuando esta en modo no fixed

--- app/src/main/java/com/projects/mirai/koukin/pruebasmapa/HelperClass/SerialLink.java
+++ app/src/main/java/com/projects/mirai/koukin/pruebasmapa/HelperClass/SerialLink.java
@@ -5,38 +5,27 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.database.sqlite.SQLiteBindOrColumnIndexOutOfRangeException;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
-import android.util.Log;
 
 import com.swiftnav.sbp.SBPMessage;
 import com.swiftnav.sbp.client.SBPCallback;
 import com.swiftnav.sbp.client.SBPHandler;
-import com.swiftnav.sbp.loggers.JSONLogger;
 import com.swiftnav.sbp.logging.MsgLog;
 import com.swiftnav.sbp.navigation.MsgPosLLH;
-
-import org.apache.commons.lang3.ObjectUtils;
-
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
 
-import javax.xml.transform.Source;
 
 public class SerialLink {
     SBPHandler piksiHandler;
     SBPDriverJ2XX piksiDriver;
     Context context;
-    private double lat;
-    private double log;
-    public int h_accuracy;
-    public int v_accuracy;
+    private double lat = 92;
+    private double log = 182;
+    public double h_accuracy;
+    public double v_accuracy;
+    public double height;
     public String type;
     private String[] fix_type_a = new String[8];
     public boolean isConnected;
@@ -66,7 +55,6 @@ public class SerialLink {
                 }
             } else {
                 System.out.println("nada conectado sin permiso");
-                Log.e(TAG, "Permission denied for device " + device);
             }
         }
         }
@@ -78,7 +66,6 @@ public class SerialLink {
         if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
             UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
             if (device != null) {
-                Log.e(TAG, "Device disconnected!");
                 System.out.println("Device disconnected!");
                 if (piksiDriver != null) {
                     piksiHandler.stop();
@@ -97,7 +84,6 @@ public class SerialLink {
         populate_fix_type();
         this.context = context;
         detect_piksi();
-
     }
 
     private void detect_piksi() {
@@ -130,32 +116,25 @@ public class SerialLink {
             piksiDriver = new SBPDriverJ2XX(context, usbdev);
             isConnected = true;
             piksiHandler = new SBPHandler(piksiDriver);
-            File logfile = new File(context.getExternalFilesDir("logs"), "logfile");
-            OutputStream logstream = new FileOutputStream(logfile);
-            piksiHandler.addCallback(new JSONLogger(logstream));
         } catch (IOException e) {
-            Log.d(TAG, e.toString());
             e.printStackTrace();
             return;
 
         }catch (Exception e) {
-            Log.e(TAG, "Error opening JSON log file: " + e.toString());
+
         }
 
         System.out.println("creando piksi");
-        Log.d(TAG, "All ready to go...");
         System.out.println("iniciando piksi");
 
         piksiHandler.addCallback(MsgLog.TYPE, new SBPCallback() {
             @Override
             public void receiveCallback(SBPMessage msg) {
             if (piksiHandler == null) {
-                Log.e(TAG, "No piksi to send to!");
                 return;
             }
             MsgLog msg_ = (MsgLog) msg;
-            System.out.print(msg_.text + ".\n");
-            //piksiHandler.send(msg);
+            System.out.print("rtk:" + msg_.text + ".\n");
             }
         });
 
@@ -163,18 +142,17 @@ public class SerialLink {
             @Override
             public void receiveCallback(SBPMessage msg) {
                 if (piksiHandler == null) {
-                    Log.e(TAG, "No piksi to send to!");
                     return;
                 }
-
                 MsgPosLLH msg_ = (MsgPosLLH) msg;
                 int fix_type = msg_.flags & 0x7;
                 type = fix_type_a[fix_type];
-                if (fix_type != 0) {
+                if (fix_type == 1) {
                     lat = msg_.lat;
                     log = msg_.lon;
                     h_accuracy = msg_.h_accuracy;
                     v_accuracy = msg_.v_accuracy;
+                    height = msg_.height;
                 }
             }
         });
@@ -185,7 +163,6 @@ public class SerialLink {
             piksiHandler.start();
             return true;
         }else{
-            System.out.println("ssssss");
             return false;
         }
     }
@@ -202,16 +179,15 @@ public class SerialLink {
     public double getLat() {
         if (piksiDriver != null) {
             return lat;
-
         }
-        return -1;
+        return 92;
     }
 
     public double getLon() {
         if (piksiDriver != null) {
             return log;
         }
-        return -1;
+        return 182;
     }
 }
 
